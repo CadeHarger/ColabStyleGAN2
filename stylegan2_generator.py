6b1c12f4d5f7234000dc6c273f882262879f1699
CadeHarger <72991181+CadeHarger@users.noreply.github.com>
2022-09-08 23:43:47 -0500

    Update stylegan2_generator.py

--- stylegan2_generator.py
+++ stylegan2_generator.py
@@ -97,19 +97,23 @@ class SynthesisNetwork(tf.keras.layers.Layer):
         
         dlatents_in = tf.cast(dlatents_in, 'float32')
         y = None
-        
         # Early layers
         x = tf.tile(tf.cast(self.const_4_4, 'float32'), [tf.shape(dlatents_in)[0], 1, 1, 1])
         x, s = self.layer_4_4(x, dlatents_in[:, 0])
         y, s1 = self.torgb_4_4(x, dlatents_in[:, 1], y)
         styles = tf.concat([s, s1], -1) 
+        shapes = [(s.shape, 0), (s1.shape, 1)]
         # Main layers
         for res in range(3, self.resolution_log2 + 1):
             x, s1 = getattr(self, 'layer_{}_{}_up'.format(2**res, 2**res))(x, dlatents_in[:, res*2-5])
+            shapes.append((s1, res*2-5))
             x, s2 = getattr(self, 'layer_{}_{}'.format(2**res, 2**res))(x, dlatents_in[:, res*2-4])
+            shapes.append((s2, res*2-4))
             y = upsample_2d(y, k=self.resample_kernel, impl=self.impl, gpu=self.gpu)
             y, s3 = getattr(self, 'torgb_{}_{}'.format(2**res, 2**res))(x, dlatents_in[:, res*2-3], y)
+            shapes.append((s3, res*2-3))
             styles = tf.concat([styles, s1, s2, s3], -1)
+        print(shapes)
         images_out = y
         return tf.identity(images_out, name='images_out'), styles
     
